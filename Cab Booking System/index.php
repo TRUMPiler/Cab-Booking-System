2e6105104b3fefbafb77ee3e4c510e49e7b42bed
Naishal Doshi <naishal036@gmail.com>
2023-10-08 19:02:10 +0530

    Add files via upload

--- Cab Booking System/index.php
+++ Cab Booking System/index.php
@@ -12,7 +12,9 @@ session_start();
   <title>Taxi Booking | Home</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
+  <!-- ajax -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+    </script>
   <!-- Favicons -->
   <link href="Images/Taxibooking.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
@@ -27,10 +29,21 @@ session_start();
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
+  <script src="js/loader.js"></script>
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
+  <style>
+    #preloader
+{
+   background: transparent url("images/loading.gif") no-repeat center;
+   backdrop-filter: blur(10px);
+   background-size: 13%;
+   height: 100vh;
+   width: 100%;
+   position: fixed;
+   z-index: 100;
+}
+  </style>
   <!-- =======================================================
   * Template Name: Ninestars
   * Updated: Jul 27 2023 with Bootstrap v5.3.1
@@ -41,7 +54,7 @@ session_start();
 </head>
 
 <body>
-
+  <div id="preloader"></div>
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
@@ -86,11 +99,11 @@ session_start();
             {
               echo "<li><a class='getstarted scrollto' href='login.php'>Login</a></li>";
             }
-            elseif($_SESSION["filename"]!="")
+            elseif(isset($_SESSION["filename"]))
             {
               echo "<li><a  href='profile1'>"."<img src='images/".$_SESSION["filename"]."' alt='".$_SESSION["fname"]."' style='border-radius:150%;'>"."</a></li>";
             }
-            elseif($_SESSION["filename"]=="" && $_SESSION["fname"]!="")
+            elseif(!isset($_SESSION["filename"]) && $_SESSION["fname"]!="")
             {
               echo "<li><a class='getstarted scrollto' href='profile1'>".$_SESSION["fname"]."</a></li>";
             }
@@ -179,13 +192,13 @@ session_start();
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
-            <div class="icon-box">
+         <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+         <a href="Request ride"><div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4 class="title"><a href="">Request Ride</a></h4>
+              <h4 class="title"><a href="Request ride">Request Ride</a></h4>
               <p class="description">Fill in your travel details and put on a request.</p>
             </div>
-          </div>
+          </div></a>
 
           <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
@@ -639,13 +652,14 @@ session_start();
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Ninestars</h3>
+            <h3>Team no 2</h3>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              Babu Madhav Institude of Information and Technology <br>
+              Uka Tarsadia University<br>
+              Bardoli, Gujrat 394620<br>
+              India <br><br>
+              <strong>Phone:</strong> +91 6353 0300 96<br>
+              <strong>Email:</strong> registrar@utu.ac.in<br>
             </p>
           </div>
 
@@ -654,13 +668,13 @@ session_start();
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#hero">Home</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#services">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#scroll service">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#services">Services</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#services">Terms of service</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#services">Privacy policy</a></li>
             </ul>
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
+          <!-- <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
@@ -669,17 +683,17 @@ session_start();
               <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
             </ul>
-          </div>
+          </div> -->
 
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Social Networks</h4>
             <!-- <p></p> -->
             <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <a href="https://twitter.com/utumalibacampus?lang=en" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="https://www.facebook.com/utu.malibacampus/" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/bmiit.utu/?hl=en" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="mailto:“dashtaxigg@gamil.com.com" class="google-plus"><i class="bx bxl-google"></i></a>
+              <a href="https://www.linkedin.com/company/utu-malibacampus" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
           </div>
 
@@ -696,7 +710,7 @@ session_start();
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
-        Designed by <a href="https://bootstrapmade.com/">Pranav Chaudhari, Naishal Doshi and Navdeep Chaudhary</a>
+        Designed by <a href="https://github.com/Whitespider06">Pranav Chaudhari, Naishal Doshi and Navdeep Chaudhary</a>
       </div>
     </div>
   </footer><!-- End Footer -->
@@ -710,7 +724,13 @@ session_start();
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-
+<script>
+// loading();
+$(document).ready(function (){
+  HH();
+}
+)
+loading();</script>
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
