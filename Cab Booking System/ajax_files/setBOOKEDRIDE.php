6edfca1696b2600f1cb3faceac4ebaaf46871d8f
Naishal Doshi <naishal036@gmail.com>
2023-11-01 01:53:26 +0530

    Add files via upload

--- Cab Booking System/ajax_files/setBOOKEDRIDE.php
+++ Cab Booking System/ajax_files/setBOOKEDRIDE.php
@@ -1,11 +1,10 @@
 <?php 
 include "../connection.php";
-$query="insert into tbl_booked(InterestID) values(".$_POST["interestid"].")";
+$query="insert into tbl_booked(InterestID,RideStatus) values(".$_POST["interestid"].",'Ride Booked')";
 $result=mysqli_query($conn,$query);
 if($result>0)
 {
     $query="DELETE FROM tbl_interest WHERE NOT InterestID=".$_POST["interestid"]." and  RequestID=".$_POST["requestid"]."";
-   
     $results=mysqli_query($conn,$query);
     if($results > 0)
     {
