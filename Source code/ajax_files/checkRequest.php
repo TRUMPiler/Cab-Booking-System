cea4ef1c80b9415b2d70ae9942ef56ee6bd93249
Naishal Doshi <naishal036@gmail.com>
2023-11-04 16:05:17 +0530

    Add files via upload
    no changes

--- Source code/ajax_files/checkRequest.php
+++ Source code/ajax_files/checkRequest.php
@@ -5,28 +5,37 @@ $query="select tbl_request_ride.Request_id from tbl_request_ride where tbl_reque
 $result=mysqli_query($conn,$query);
 if($result->num_rows > 0)
 {
+    
     $query="select tbl_request_ride.Request_id from tbl_request_ride JOIN tbl_interest where tbl_request_ride.passengerId=".$_SESSION["id"]." and tbl_interest.RequestID=tbl_request_ride.Request_id;";
     $resultss=mysqli_query($conn,$query);
     
     if($resultss->num_rows >  0)
-    {
+    {   
         
         $query="SELECT tbl_request_ride.Request_id From tbl_booked JOIN tbl_request_ride JOIN tbl_interest where tbl_request_ride.passengerId=".$_SESSION["id"]." and tbl_interest.RequestID=tbl_request_ride.Request_id and tbl_booked.InterestID=tbl_interest.interestID and Not tbl_booked.RideStatus in ('Ride Completed','Ride Cancelled');";
         $results=mysqli_query($conn,$query);
-        if($results->num_rows == 0)
+        if($results->num_rows ==0)
         { 
-           
+          
+            
+        }
+        else
+        {
             echo "<script>alert('You already have Requested for Ride')</script>"; 
             echo "<script>window.location='Booked Ride'</script>"; 
         }
     }
     else
     {
-       
+            
         echo "<script>alert('You already have Requested for Ride')</script>"; 
         echo "<script>window.location='Booked Ride'</script>"; 
     }
    
 
+}
+else
+{
+    echo "2";
 }
 ?>
--- Source code/ajax_files/checkrr.php
+++ Source code/ajax_files/checkrr.php
@@ -1,7 +1,7 @@
 <?php 
 
 include "connection.php"; 
-$query="select RequestID,driver.id from tbl_interest JOIN driver where tbl_interest.DriverID=".$_SESSION["id"]." and driver.id=tbl_interest.DriverID";
+$query="select RequestID,driver.id from tbl_interest JOIN tbl_booked JOIN driver where tbl_interest.DriverID=".$_SESSION["id"]." and driver.id=tbl_interest.DriverID and tbl_booked.Booked_ID=tbl_interest.interestID and NOT tbl_booked.RideStatus in ('Ride Completed','Ride Cancelled');";
 $result=mysqli_query($conn,$query);
 if($result->num_rows>0)
 {
