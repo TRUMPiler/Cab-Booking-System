808bee375f172e885b89910e50bea13f602d1264
Naishal Doshi <naishal036@gmail.com>
2023-11-06 04:30:19 +0530

    Update checkmail1.php

--- Source code/ajax_files/checkmail1.php
+++ Source code/ajax_files/checkmail1.php
@@ -1,39 +1,54 @@
 <?php
 session_start();
 require_once "../connection.php";
+
 if(isset($_POST["email"]))
 {
-    $query="select email from passenger where email='".$_POST["email"]."' or contact=".$_POST["contact"]."";
+    $query="select email from passenger where email='".$_POST["email"]."' and not email='".$_SESSION["email"]."'";
     $ret=mysqli_query($conn,$query);
     if($ret->num_rows>0)
     {
         $_SESSION["emailver"]="false";
-        echo "./emailerror.php";
+        // echo "<h5>Email already exits</h5>";
+        // echo "<div class='GG'><a href='login2'>click to go to login page</a></div>";
+        // echo "<script>
+        //                                 HH();
+        //                                 </script>";
+       
+        echo "false";
         exit();
     }
     else
     {
-        $query="select email from driver where email='".$_POST["email"]."' or contact=".$_POST["contact"]."";
+      
+        $query="select email from driver where email='".$_POST["email"]."' and not email='".$_SESSION["email"]."'";
         $ret=mysqli_query($conn,$query);
         if($ret->num_rows>0)
         {
             $_SESSION["emailver"]=false;
-            echo "./emailerror.php";
+            echo "false";
             exit();
         }
         else
         {
-            $query="select email from admin where email='".$_POST["email"]."' or  contact=".$_POST["contact"]."";
+          
+            $query="select email from admin where email='".$_POST["email"]."' and not email='".$_SESSION["email"]."'";
             $ret=mysqli_query($conn,$query);
             if($ret->num_rows>0)
             {
                 $_SESSION["emailver"]=false;
-                echo "./emailerror.php";                        
+                echo "false";                        
                 exit();
             }
             else
             {
+              
                 $_SESSION["emailver"]=true;
+             
+                
+                
+                           
+            
             }
         }
     }
