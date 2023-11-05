afa36703c17f280ce2e707fcc197cd23609a17cf
Naishal Doshi <naishal036@gmail.com>
2023-11-06 01:10:13 +0530

    Update MakePayment.php

--- Source code/MakePayment.php
+++ Source code/MakePayment.php
@@ -1,22 +1,9 @@
-<?php 
-session_start();
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <!-- <script src="js/loader.js"></script> -->
-    <title>Payment</title>
-    <link href="Images/Taxibooking.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
+    <title>Document</title>
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -27,53 +14,16 @@ session_start();
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
     <style>
-        #preloader {
-            background: transparent url("images/loading.gif") no-repeat center;
-            backdrop-filter: blur(10px);
-            background-size: 13%;
-            height: 100vh;
-            width: 100%;
-            position: fixed;
-            z-index: 100;
+        .form-control:focus {
+            border-color: rgb(255, 174, 0);
+            /* Border color when focused */
         }
-
-        .error span {
-            color: red;
-
-        }
-
-        span.error {
-            color: red;
-            border-radius: 2px solid red;
-        }
-
-        body {
-            background-image: url('Images/Background.jpg');
-            /* Set the URL of your background image */
-            background-size: cover;
-            /* Cover the entire viewport with the image */
-            background-repeat: no-repeat;
-            /* Prevent image repetition */
-            color: rgb(255, 174, 0);
-            /* Set text color to white for better visibility */
-        }
-
-        .container {
-            /* background: rgba(255, 255, 255, 0.8); */
-            border-radius: 10px;
-            padding: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
         .card {
             border-radius: 20px;
             width: 500px;
         }
-
-        .custom-button {
+        .razorpay-payment-button
+        {
             background: white;
             /* White background */
             color: rgb(255, 174, 0);
@@ -82,32 +32,42 @@ session_start();
             border-radius: 5px;
             padding: 10px 20px;
             cursor: pointer;
-        }
 
-        /* Button hover effect using Bootstrap classes */
-        .custom-button:hover {
+        }
+        .razorpay-payment-button:hover {
+
             background: rgb(255, 174, 0);
             /* Background color on hover */
             color: white;
             /* Text color on hover */
         }
-
-        .form-control:focus {
-            border-color: rgb(255, 174, 0);
-            /* Border color when focused */
-        }
     </style>
 </head>
 
-<body>
-    <div class="container">
-        <div class="row justify-content-start">
-            <div class="col-md-8">
+<body onload="document.getElementsByClassName('razorpay-payment-button').click();" style="background-image: url('Images/Background.jpg'); background-size: cover; background-repeat: no-repeat;color: rgb(255, 174, 0);">
+<?php session_start();
+    include("connection.php");
+    $query="select * from passenger where id=".$_SESSION['id']."";
+    $result=mysqli_query($conn,$query);
+    $row=mysqli_fetch_array($result);?>
+     <div class="card-body">
+     <div class="row justify-content-start">
+     <div class="col-md-8">
                 <div class="card">
-                    <h1 class="card-header text-center">Payment</h1>
+                <h1 class="card-header text-center">Payment</h1>
                     <div class="card-body">
-                        <form id="myform">
-                            <?php 
+                    <section class="registration-section text-center" id="personal-info-section">
+                    <div class="form-row my-5">
+                                    <div class="form-group col-md-12">
+                                        <label for="description">Your Payable Amount</label>
+                                    </div>
+                    </div>
+                    <div class="form-group col-md-12">
+                                       
+                                        <!-- <textarea class="form-control" id="description" name="description" placeholder="Provide us your feedback regarding our travel services.." rows="4"></textarea> -->
+                                    </div> 
+<form action="<?php echo "index";?>" method="POST">
+<?php 
                             include "./connection.php";
                             $query="select tbl_interest.Cost from tbl_booked JOIN tbl_interest JOIN tbl_request_ride WHERE tbl_request_ride.passengerId=56 and tbl_interest.RequestID=tbl_request_ride.Request_id and tbl_booked.InterestID=tbl_interest.interestID limit 1";
                             $result=mysqli_query($conn,$query);
@@ -116,40 +76,44 @@ session_start();
                                 while($row=$result->fetch_array())
                                 {
                             ?>
-                            <section class="registration-section text-center" id="personal-info-section">
-                                <h1>₹<?php echo $row["Cost"]?></h1>
-                                <!-- Amount will be displayed here -->
-                                <div class="form-row my-5">
-                                    <div class="form-group col-md-12">
-                                        <label for="description">Your Payable Amount</label>
-                                        <!-- <textarea class="form-control" id="description" name="description" placeholder="Provide us your feedback regarding our travel services.." rows="4"></textarea> -->
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-center">
-                                    <!-- <a href="index"><button type="button" class="btn custom-button">Home</button></a> -->
-                                   <button  name="submit" id="pay" class="btn custom-button">Pay</button> 
-                                    <!-- Button that user will click to make the payment -->
-                                </div>
-                            </section>
-                            <?php 
-                                }
-                            }
-                            ?>
-                            <script>
-                                $(document).ready(function(){
-                                    $("#myform").submit(function(event)
-                                    {
-                                        event.preventDefault();
-                                        window.location='booked Ride';
-                                    })
-                                })
-                            </script>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+                            <h1 id='razorGG'>₹<?php echo $row["Cost"]?></h1>
+<script
+    src="https://checkout.razorpay.com/v1/checkout.js"
+    data-key="rzp_test_jeRt2IQyrN9mmp" 
+    data-amount="100" 
+    data-currency="INR"
+    data-order_id=''
+    data-buttontext="Pay ₹<?php echo $row["Cost"]?>"
+    data-name="TAXDASH"
+    data-description="TAXDASH"
+    data-image="Images/Taxibooking.png"
+    data-prefill.name=<?php echo "'".$_SESSION['fname']."'";?>
+    data-prefill.email=<?php echo "'".$_SESSION['email']."'";?>
+    data-theme.color="#F37254"
+>
+function init(){
+    $('.razorpay-payment-button').click(function(){
+        window.location='index';
+    });
+}
 
+
+</script>
+<input type="hidden" custom="Hidden Element" name="hidden"/>
+<?php 
+}
+}?>
+
+</form>
+</div>
+</div>
+</div>
+                    </section>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
 </html>
