554def584a75a4d75857641608fcdbbbce607f0d
Naishal Doshi <naishal036@gmail.com>
2023-11-05 17:15:16 +0530

    Update viewingInterest.php

--- Source code/viewingInterest.php
+++ Source code/viewingInterest.php
@@ -11,6 +11,7 @@ if(isset($_SESSION["fname"]))
 include "connection.php";
 $query="SELECT rr.interestID, rr.RequestID, driver.fname AS DriverName, 
 vehicle.company_name AS VehicleCompany, 
+driver.id as DriverID,
 vehicle.model AS Model, rr.Cost AS Cost, 
 COALESCE(tbl_booked.RideStatus, 'Not Booked') AS RideStatus, driver.image AS Image 
 FROM tbl_interest AS rr 
@@ -18,7 +19,7 @@ JOIN tbl_request_ride AS trr ON rr.RequestID = trr.Request_id
 JOIN driver ON rr.DriverID = driver.id 
 JOIN vehicle ON driver.id = vehicle.driver_id 
 LEFT JOIN tbl_booked ON rr.interestID = tbl_booked.InterestID 
-WHERE trr.passengerId = ".$_SESSION["id"]." AND (tbl_booked.RideStatus IS NULL OR tbl_booked.RideStatus NOT IN ('Ride Completed', 'Ride Booked'));";
+WHERE trr.passengerId = ".$_SESSION["id"]." AND (tbl_booked.RideStatus IS NULL OR tbl_booked.RideStatus NOT IN ('Ride Completed', 'Ride Booked','Ride Cancelled'));";
 $result=mysqli_query($conn,$query);
 // echo $query;
 ?>
