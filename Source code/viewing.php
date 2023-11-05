73afa2634a0cfc1b8757a4a90afcb7048231c50e
Naishal Doshi <naishal036@gmail.com>
2023-11-05 14:29:04 +0530

    Update viewing.php
    gitch fixed

--- Source code/viewing.php
+++ Source code/viewing.php
@@ -9,19 +9,17 @@ if(isset($_SESSION["fname"]))
 }
 
 include "connection.php";
-$query="SELECT Request_id, SourceAddress,DestinationAddress,rr.From,rr.To, 
-passenger.fname as passengername, 
+$query="SELECT rr.Request_id, rr.SourceAddress, rr.DestinationAddress, 
+rr.From, rr.To, passenger.fname AS passengername, 
 source_city.City_name AS source_city_name, 
 destination_city.City_name AS destination_city_name 
 FROM tbl_request_ride AS rr 
 JOIN tbl_city AS source_city ON rr.SourceCity = source_city.CityID 
-JOIN tbl_city AS destination_city ON rr.DestinationCity = destination_city.CityID 
-JOIN passenger as passenger on rr.passengerId=passenger.id 
-JOIN tbl_interest 
-JOIN tbl_booked 
-where SourceCity=".$_SESSION["city"]." 
-AND NOT (tbl_interest.RequestID=rr.Request_id 
-and tbl_booked.InterestID=tbl_interest.InterestID);";
+JOIN tbl_city AS destination_city ON rr.DestinationCity = destination_city.CityID \
+JOIN passenger ON rr.passengerId = passenger.id 
+LEFT JOIN tbl_interest ON tbl_interest.RequestID = rr.Request_id 
+LEFT JOIN tbl_booked ON tbl_booked.InterestID = tbl_interest.InterestID 
+WHERE rr.SourceCity = ".$_SESSION["city"]." AND (tbl_interest.RequestID IS NULL OR tbl_booked.InterestID IS NULL);";
 $result=mysqli_query($conn,$query);
 
 ?>
