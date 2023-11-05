c2a2862bfdb44ea3476a7f94e5274e448e9ac998
Naishal Doshi <naishal036@gmail.com>
2023-11-06 02:22:13 +0530

    Update makepayment.php

--- Source code/ajax_files/makepayment.php
+++ Source code/ajax_files/makepayment.php
@@ -1,13 +1,25 @@
 <?php 
 include "../connection.php";
-$query="update tbl_booked set RideStatus='Payment Done' where Booked_ID=".$_POST["booked_id"]."";
+$_SESSION["booked_id"];
+$transcation=date('Ymd').random_int(10000,99999);
+$query="insert into tbl_payment(`Transactionid`, `BookedID`) values('$transcation',".$_SESSION["booked_id"].")";
 $result=mysqli_query($conn,$query);
 if($result>0)
 {
-    echo "true";
+    $query="update tbl_booked set RideStatus='Payment Done' where Booked_ID=".$_SESSION["booked_id"]."";
+$results=mysqli_query($conn,$query);
+if($results>0)
+{
+    echo "<script>window.location='index'</script>";
 }
 else
 {
     echo "false";
 }
+}
+else
+{
+    echo "false";
+}
+
 ?>
