5d6d0aa02c88c239484b1e5c736a2adfcb6d03b6
Naishal Doshi <naishal036@gmail.com>
2023-11-06 05:46:54 +0530

    Update profile passenger.php

--- Source code/profile passenger.php
+++ Source code/profile passenger.php
@@ -68,12 +68,14 @@ session_start();
         <div class="row">
             <?php
             $conn = new mysqli("localhost", "root", "", "cms");
+
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
 
+
             // $sql = "SELECT * from ".$_SESSION["role"]." where fname='".$_SESSION["fname"]."' and password='".$_SESSION ["password"]."' limit 1";
-            $sql = "SELECT id,fname,mname,lname,password,dob,gender,contact,address,email,image,tbl_city.City_Name from passenger Join tbl_city where  id=".$_SESSION["id"]." and tbl_city.CityID=passenger.CityGG limit 1;";
+            $sql = "SELECT id,fname,mname,lname,password,dob,gender,contact,address,email,image,tbl_city.City_Name from driver Join tbl_city where id=".$_SESSION["id"]." and tbl_city.CityID=driver.CityGG limit 1;";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
@@ -82,7 +84,7 @@ session_start();
                 <div class="col-md-3 border-right">
                     <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="img-fluid img-square mt-5" width="150px" src="Images/<?php echo $row['image']; ?>"><span class="font-weight-bold"><?php echo $row['fname'] . " " . $row['lname']; ?></span><span class="text-black-50"><?php echo $row['email']; ?></span><span> </span></div>
                 </div>
-                <div class="col-md-8 border-right">
+                <div class="col-md-5 border-right">
                     <div class="p-3 py-5">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <h4 class="text-right">Personal Details: </h4>
@@ -97,32 +99,12 @@ session_start();
                                 <div class="col-md-12"><label class="labels">Contact Number</label><input type="text" class="form-control readonly" name="contact" readonly value="<?php echo $row['contact']; ?>"></div>
                                 <div class="col-md-12"><label class="labels">Address</label><input type="text" class="form-control readonly" name="address" readonly value="<?php echo $row['address']; ?>"></div>
                                 <div class="col-md-12"><label class="labels">Date of Birth</label><input type="text" class="form-control readonly" name="dob" readonly value="<?php echo $row['dob']; ?>"></div>
-                                <div class="col-md-12"><label class="labels">Gender</label><select class="form-control readonly" id="gender" name="gender" readonly>
-                                            <?php if($row["gender"]=="male" || $row["gender"]=="Male")
-                                            {
-                                                echo "<option value='male' selected>Male</option>";
-                                                echo "<option value='female'>Female</option>
-                                                <option value='other'>Other</option>";
-                                            }
-                                            else if($row["gender"]=="female" || $row["gender"]=="Female")
-                                            {
-                                                echo "<option value='male'>Male</option>";
-                                                echo "<option value='female' Selected>Female</option>
-                                                <option value='other'>Other</option>";
-                                            }
-                                            else
-                                            {
-                                                echo "<option value='male' >Male</option>";
-                                                echo "<option value='female'>Female</option>
-                                                <option value='other' selected>Other</option>";
-                                            }
-                                            ?>
-                                            
-                                        </select></div>
+                                <div class="col-md-12"><label class="labels">Gender</label><input type="text" class="form-control readonly" name="gender" readonly value="<?php echo $row['gender']; ?>"></div>
                                 <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control readonly" name="email" readonly value="<?php echo $row['email']; ?>"></div>
                                 <!-- <div class="col-md-12"><label class="labels">Password</label><input type="text" class="form-control readonly" name="password" readonly value="<?php echo $row['password']; ?>"></div> -->
-                                <!-- <div class="col-md-12"><label class="labels">Profile</label><input type="text" class="form-control readonly" name="role" readonly placeholder="Passenger/Driver" value="passenger"></div> -->
+                                <!-- <div class="col-md-12"><label class="labels">Profile</label><input type="text" class="form-control readonly" name="role" readonly placeholder="Passenger/Driver" value="driver"></div> -->
                             <?php
+                            
                         }
                         $conn->close();
                             ?>
@@ -131,29 +113,29 @@ session_start();
                                 <div class="col-md-6"><label class="labels">Country</label><input type="text" class="form-control " name="country" readonly value="India"></div>
                                 <div class="col-md-6"><label class="labels">State</label><input type="text" class="form-control " name="state" readonly value="<?php echo $row["City_Name"]?>"></div>
                             </div>
-                        <div class="row mt-5">
-                            <div class="col-md-3">
-                                <div class="text-center">
-                                    <a href="index"><button class="btn btn-primary profile-button" name="update" type="button">Home</button></a>
+                            <div class="row mt-5">
+                                <div class="col-md-3">
+                                    <div class="text-center">
+                                        <button class="btn btn-primary profile-button" name="update" type="button">Home</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-3">
+                                <div class="col-md-3">
+                                    <div class="text-center">
+                                        <button class="btn btn-primary profile-button" name="update" type="button" onclick="enableFields()">Edit</button>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
                                 <div class="text-center">
                                     <a href="index"><button class="btn btn-primary profile-button" name="logout" id="logout" type="submit">Log out</button></a>
                                 </div>
                             </div>
-                            <div class="col-md-3">
-                                <div class="text-center">
-                                    <button class="btn btn-primary profile-button" name="update" type="button" onclick="enableFields()">Edit</button>
+                                <div class="col-md-6">
+                                    <div class="text-center">
+                                        <button class="btn btn-primary profile-button" name="update"id="update" type="submit">Save Personal Details</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-3">
-                                <div class="text-center">
-                                    <button class="btn btn-primary profile-button"  id="update" name="update" type="submit">Save Personal Details</button>
-                                </div>
-                            </div>
-                        </div>
-                        <script>
+                            <script>
                             $(document).ready(function() {
                                 $("#update").click(function () {
                                     $("#myform").submit(function(event)
@@ -162,7 +144,7 @@ session_start();
                                             var formdata=new FormData(this);
                                             $.ajax({
                                                 type:"POST",
-                                                url:"ajax_files/updatepassenger.php",
+                                                url:"ajax_files/updatedriver.php",
                                                 data:formdata,
                                                 contentType: false,
                                                 cache: false,
@@ -176,7 +158,7 @@ session_start();
                                                     else if(data=="false")
                                                     {
                                                         alert("Profile updation failed email already exits")
-                                                        window.location="profile passenger";
+                                                        window.location="profile driver";
                                                     }
                                                     else
                                                     {
@@ -188,13 +170,14 @@ session_start();
                                         });
                                     
                                 })
+                                
                                         $("#logout").click(function(){
                                             $.post("logoutGG.php",function(data)
                                             {
                                                 if(data=="true")
                                                 {
                                                     alert("successfully logged out");
-                                                    window.location='index';
+                                                    window.location='index_driver';
                                                 }
                                                 else
                                                 {
@@ -207,14 +190,92 @@ session_start();
                         </form>
                     </div>
                 </div>
+                <div class="col-md-4">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center experience">
+                            <h4>Vehicle Details:</h4>
+                        </div><br>
+                        <form id="myformvehicle">
+                        <?php
+                        $conn = new mysqli("localhost", "root", "", "cms");
 
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+
+                        // $sql = "SELECT * from ".$_SESSION["role"]." where fname='".$_SESSION["fname"]."' and password='".$_SESSION ["password"]."' limit 1";
+                        $sql = "SELECT * from vehicle where driver_id=".$_SESSION["id"]." limit 1";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            $row = $result->fetch_assoc();
+                        ?>
+                            <div class="col-md-12"><label class="labels">Company:</label><input type="text" class="form-control readonly" name="company" readonly value="<?php echo $row['company_name']; ?>"></div> <br>
+                            <div class="col-md-12"><label class="labels">Vehicle name:</label><input type="text" class="form-control readonly" name="vname" readonly value="<?php echo $row['model']; ?>"></div> <br>
+                            <div class="col-md-12"><label class="labels">Vehicle Plate number:</label><input type="text" class="form-control readonly" name="platenumber" readonly value="<?php echo $row['vehicle-number']; ?>"></div>
+                            <div class="row mt-5">
+                                <div class="col-md-3">
+                                    <div class="text-center">
+                                        <button class="btn btn-primary profile-button" name="update" type="button" onclick="enableFields()">Edit</button>
+                                    </div>
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="text-center">
+                                        <button class="btn btn-primary profile-button" id="update1" name="updatevehicle" type="submit">Save Personal Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <script>
+                            $(document).ready(function() {
+                                $("#update1").click(function () {
+                                    $("#myformvehicle").submit(function(event)
+                                        {   
+                                            event.preventDefault();
+                                            var formdata=new FormData(this);
+                                            $.ajax({
+                                                type:"POST",
+                                                url:"ajax_files/updatevehicle.php",
+                                                data:formdata,  
+                                                contentType: false,
+                                                cache: false,
+                                                processData:false,
+                                                success:function(data){
+                                                    if(data=="true")
+                                                    {
+                                                        
+                                                        alert("vehicle updated");
+                                                        window.location='index_driver';
+                                                    }
+                                                    else if(data=="vehicle exists")
+                                                    {
+                                                        alert("vehicle updation failed vehicle number already exits")
+                                                        window.location="profile driver";
+                                                    }
+                                                    else
+                                                    {
+                                                        alert(data);
+                                                    }
+                                                },
+                                            }); 
+                                            
+                                        });
+                                    
+                                })
+                                
+                                       
+                            })
+                        </script>
+                        </form>
+                    </div>
+                <?php
+                        }
+                        // $conn->close();
+                ?>
+                </div>
         </div>
     </div>
     </div>
     </div>
-    <script>
-
-    </script>
     <script>
         // JavaScript function to enable input fields
         function enableFields() {
