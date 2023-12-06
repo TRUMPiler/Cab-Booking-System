14ca2dd6eb77f0fe410561f07b3264c9f5b58ba6
Whitespider06 <130541975+Whitespider06@users.noreply.github.com>
2023-12-06 15:00:55 +0530

    Add files via upload

---
+++ otp.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if(isset($_SESSION["verified"]))
+{
+    if($_SESSION["verified"]==true)
+    {
+        header("location:index");
+    }
+}
+if(isset($_SESSION["email"]))
+{
+    $otp=random_int(10000,99999);
+    $to_email = $_SESSION["email"];
+    $subject = "OTP VERIFICATION";
+    $body = "
+    Hey welcome to taxi management system \n
+    we hope you are enjoying your experience\n
+    your otp is $otp\n 
+    please insert it for further completion of the process";
+    $headers = "From: dashtaxigg@gmail.com";
+
+    if (mail($to_email, $subject, $body, $headers)) {
+        $_SESSION["otp"]=$otp;
+        header("location:process");
+    } 
+    else 
+    {
+       header("error.php");
+    }
+}
+else
+{
+    header("location:index");
+}
+
+?>
---
+++ setinterest.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+
+include "ajax_files/checkrr.php";
+include "connection.php";
+$query="SELECT * FROM `driver`where id=".$_SESSION["id"]."";
+// include "connection.php";
+$result=mysqli_query($conn,$query);
+if($result->num_rows>0 && $_SESSION["rrveri"]==true)
+{
+    while($row=$result->fetch_array())
+    {
+        $query="insert into tbl_interest(RequestID,DriverID,Cost,date_of_request) values(".$_SESSION["RequestID"].",".$row["id"].",'".$_POST["cost_estimation"]."',CURRENT_TIMESTAMP)";
+        $results=mysqli_query($conn,$query);
+        echo "true";
+    }
+}
+else
+{
+  
+    echo "false";
+}
+?>
