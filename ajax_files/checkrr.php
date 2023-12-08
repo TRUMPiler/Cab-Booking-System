ec314868a6b161c167e25df80378cd29809f9ac0
Naishal Doshi <naishal036@gmail.com>
2023-12-08 14:04:59 +0530

    Add files via upload

--- ajax_files/checkrr.php
+++ ajax_files/checkrr.php
@@ -1,7 +1,23 @@
 <?php 
 
 include "connection.php"; 
-$query="select RequestID,driver.id from tbl_interest JOIN tbl_booked JOIN driver where tbl_interest.DriverID=".$_SESSION["id"]." and driver.id=tbl_interest.DriverID and tbl_booked.Booked_ID=tbl_interest.interestID and NOT tbl_booked.RideStatus in ('Ride Completed','Ride Cancelled');";
+$query="SELECT 
+tbl_interest.RequestID,
+driver.id AS driver_id
+FROM 
+tbl_interest
+JOIN 
+driver ON driver.id = tbl_interest.vehicle_id  -- Updated foreign key to vehicle_id
+JOIN 
+vehicle ON tbl_interest.vehicle_id = vehicle.id
+JOIN 
+tbl_booked ON tbl_booked.Booked_ID = tbl_interest.interestID
+WHERE 
+driver.id = ".$_SESSION["id"]." 
+AND NOT tbl_booked.RideStatus IN ('Ride Completed','Ride Cancelled');
+
+";
+// echo $query;
 $result=mysqli_query($conn,$query);
 if($result->num_rows>0)
 {
